e0b619c1c2775c55657e56ced48a9425d571e448
Atef Ben Ali <atef.bettaib@gmail.com>
2019-12-21 18:27:32 +0100

    style: clean-up `src/index.js` file

--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
-class Square extends React.Component {
-  render() {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square() {
     return (
       <button className="square">
         {/* TODO */}
       </button>
     );
-  }
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
+function Board() {
+    function renderSquare(i) {
+      return <Square />;
+    }
 
-  render() {
     const status = 'Next player: X';
 
     return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
     );
-  }
 }
 
-class Game extends React.Component {
-  render() {
+function Game() {
     return (
       <div className="game">
         <div className="game-board">
@@ -52,13 +51,10 @@ class Game extends React.Component {
         </div>
       </div>
     );
-  }
 }
 
-// ========================================
 
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-
