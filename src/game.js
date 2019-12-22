738a408828750ec30b01e88dfa82c76dff8c98e5
Atef Ben Ali <atef.bettaib@gmail.com>
2019-12-22 13:52:17 +0100

    feat: pass `squares` prop from parent component `game` to `board` component

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Board from "./board";
+import calculateWinner from "./calculate-winner";
 
 export default function Game() {
     var [history, setHistory] = React.useState([{
@@ -7,13 +8,29 @@ export default function Game() {
     }]);
     var [xIsNext, setXIsNext] = React.useState(true);
 
+    const gameHistory = history;
+    const current = gameHistory[gameHistory.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+    
+    function handleClick(i) {}
+    
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
