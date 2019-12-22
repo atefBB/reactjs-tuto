c1ae4a5008535b76b8f8a5681af3d0faf77433a6
Atef Ben Ali <atef.bettaib@gmail.com>
2019-12-22 13:42:12 +0100

    feat: add `history`/`xIsNext` states in `game` component

--- src/game.js
+++ src/game.js
@@ -2,6 +2,11 @@ import React from "react";
 import Board from "./board";
 
 export default function Game() {
+    var [history, setHistory] = React.useState([{
+        squares: Array(9).fill(null),
+    }]);
+    var [xIsNext, setXIsNext] = React.useState(true);
+
     return (
       <div className="game">
         <div className="game-board">
